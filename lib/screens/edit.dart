0e8a1b3b598e44b39e6db8dc987fd5823ec7a224
shhong94 <shhong94@naver.com>
2021-10-01 00:30:19 +0900

    home page list view

--- lib/screens/edit.dart
+++ lib/screens/edit.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:my_first_flutter_project/database/db.dart';
+import 'package:my_first_flutter_project/database/memo.dart';
+import 'package:crypto/crypto.dart';
+import 'dart:convert'; // for the utf8.encode method
+
 
 class EditPage extends StatelessWidget {
+  String title = "";
+  String text = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,7 +19,7 @@ class EditPage extends StatelessWidget {
           ),
           IconButton(
             icon: Icon(Icons.save),
-            onPressed: (){},
+            onPressed: saveDb,
           ),
         ],
       ),
@@ -22,6 +29,7 @@ class EditPage extends StatelessWidget {
         child: Column(
           children: <Widget>[
             TextField(
+              onChanged: (String title){this.title = title;},
               style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
               keyboardType: TextInputType.multiline,
               maxLines: null,
@@ -32,6 +40,7 @@ class EditPage extends StatelessWidget {
             ),
             Padding(padding: EdgeInsets.all(10)),
             TextField(
+              onChanged: (String text){this.text = text;},
               keyboardType: TextInputType.multiline,
               maxLines: null,
               decoration: InputDecoration(
@@ -44,4 +53,29 @@ class EditPage extends StatelessWidget {
 
     );
   }
+
+  Future<void> saveDb() async {
+    DBHelper sd = DBHelper();
+
+    var fido = Memo(
+      id: str2sha512(DateTime.now().toString()),
+      title: this.title,
+      text: this.text,
+      createTime: DateTime.now().toString(),
+      editTime: DateTime.now().toString(),
+    );
+    
+    await sd.insertMemo(fido);
+    print(await sd.memos());
+  }
+
+
+  String str2sha512(String text) {
+    var bytes = utf8.encode(text); // data being hashed
+
+    var digest = sha512.convert(bytes);
+    return digest.toString();
+  }
+
+
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'edit.dart';
+import '../database/memo.dart';
+import '../database/db.dart';
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -13,34 +15,113 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        physics: BouncingScrollPhysics(),
+      body: Column(
         children: <Widget>[
-          Row(
-            children: <Widget>[
-              Padding (
-                padding: EdgeInsets.only(left: 20, top: 20, bottom: 20),
-                child: Text("메모메모", style: TextStyle(fontSize: 36, color: Colors.blue),),
-              )
-            ],
-          ),
-          ...loadMemo()
+          Padding(
+              padding: EdgeInsets.only(left: 10, top: 30, bottom: 20),
+              child: Container(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "메모메모",
+                  style: TextStyle(fontSize: 36, color: Colors.blue),
+                ),
+              )),
+          Expanded(child: memoBuilder()),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
-          onPressed: (){
-            Navigator.push(context, CupertinoPageRoute(builder: (context) => EditPage()));
-          },
-          label: Text("메모 추가"),
-          tooltip: "메모 추가하려면 버튼을 누르세요",
-          icon: Icon(Icons.add),),
-
+        onPressed: () {
+          Navigator.push(
+              context, CupertinoPageRoute(builder: (context) => EditPage()));
+        },
+        label: Text("메모 추가"),
+        tooltip: "메모 추가하려면 버튼을 누르세요",
+        icon: Icon(Icons.add),
+      ),
     );
   }
 
-  List<Widget> loadMemo() {
+  List<Widget> loadMemo2() {
     List<Widget> memoList = [];
     memoList.add(Container(color: Colors.red, height: 100));
     return memoList;
+  }
+
+  Future<List<Memo>> loadMemo() async {
+    DBHelper sd = DBHelper();
+    return await sd.memos();
+  }
+
+  Widget memoBuilder() {
+    return FutureBuilder(
+      builder: (context, projectSnap) {
+        // 홈 화면에 메모가 없으면
+        if (projectSnap.data.isEmpty) {
+          return Container(
+            alignment: Alignment.center,
+            child: Text("메모를 추가해보세요"),
+          );
+        }
+        return ListView.builder(
+          padding: EdgeInsets.all(10),
+          physics: BouncingScrollPhysics(),
+          itemCount: projectSnap.data.length,
+          itemBuilder: (context, index) {
+            Memo memo = projectSnap.data[index];
+            return Container(
+              margin: EdgeInsets.all(5),
+              padding: EdgeInsets.all(10),
+              height: 100,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(
+                  color: Colors.blue,
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [BoxShadow(color: Colors.grey, blurRadius: 2)],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text(
+                        memo.title,
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w600),
+                      ),
+                      Text(
+                        memo.text,
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Text(
+                        "최종 수정 시간: " + memo.editTime.split(".")[0],
+                        style: TextStyle(fontSize: 12,),
+                        textAlign: TextAlign.end,
+                      ),
+                    ],
+                  ),
+                  // 화면에 표시할 데이터
+                ],
+              ),
+            );
+          },
+        );
+      },
+      future: loadMemo(),
+    );
   }
 }
